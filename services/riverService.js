2f2c44e771014b6c43a614b7e679158de2cfb78a
TRexTech-Lab <toshio.terano@gmail.com>
2026-02-28 14:14:16 +0900

    Update riverService.js

--- services/riverService.js
+++ services/riverService.js
@@ -18,15 +18,36 @@ async function getCurrentWaterLevel() {
     `https://www.river.go.jp/kawabou/file/files/tmlist/stg/${date}/${time}/${OBS_ID}.json`;
 
   const response = await axios.get(dataUrl);
+  const values = dataRes.data.min10Values.filter(v => v.stg !== null);
 
-  console.log(response.data);
+  const labels = values.map(v => v.obsTime).reverse();
+  const data = values.map(v => v.stg).reverse();
 
-  const values = response.data.values || response.data;
-
-  return {
-    labels: values.map(v => v.obsTime),
-    data: values.map(v => v.stg)
-  };
+  res.send(`
+    <html>
+    <head>
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    </head>
+    <body>
+      <h2>River Water Level</h2>
+      <canvas id="myChart"></canvas>
+      <script>
+        const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ${JSON.stringify(labels)},
+            datasets: [{
+              label: 'Water Level (m)',
+              data: ${JSON.stringify(data)},
+              borderWidth: 2
+            }]
+          }
+        });
+      </script>
+    </body>
+    </html>
+  `);
 }
 
 async function getWeekData() {
