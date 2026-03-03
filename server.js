2eefd21b52cb621536be1a1f69f36ab80dc1a8ba
TRexTech-Lab <toshio.terano@gmail.com>
2026-03-04 00:25:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const riverRouter = require("./routes/river");
+const cronRouter = require("./routes/cron");
 
 app.use(express.json());
 app.use("/", riverRouter);
+app.use("/cron", cronRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
