6ea8204f81b6c53006af7af22f638223b7706e88
TRexTech-Lab <toshio.terano@gmail.com>
2026-03-04 21:37:11 +0900

    Update river.js

--- routes/river.js
+++ routes/river.js
@@ -21,7 +21,7 @@ router.get("/waterlevel", async (req, res) => {
     const current10min = await riverService.getCurrentWaterLevel10min(obsId);
     const currentHour  = await riverService.getCurrentWaterLevelHour(obsId);
     const week         = await riverService.getWeekData(obsId);
-    const sixMonth   = await riverService.getSixMonthDataFromDB(obsId);
+    const sixMonth   = await getSixMonthDataFromDB(obsId);
 
     if (req.query.json) {
       return res.json({ current10min, currentHour, week, sixMonth });
