350bb52bb5803a52a39cfda9df4dc0f659e6a983
TRexTech-Lab <toshio.terano@gmail.com>
2026-03-07 13:57:17 +0900

    Update cron.js

--- routes/cron.js
+++ routes/cron.js
@@ -24,8 +24,9 @@ router.get("/save", async (req, res) => {
       try {
         // --- weekData を取得 ---
         const weekData = await getWeekData(p.obs_id);
-        // console.log("weekData sample:", weekData);
-
+        //console.log("weekData sample:", weekData);
+        console.log("DEBUG", p.obs_id, weekData.labels?.length);
+        
         const labels = weekData.labels || [];
         const data = weekData.data || [];
 
